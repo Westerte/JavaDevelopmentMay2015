357a589f129dbe78a741a3ce13d0ca92eb2a008e
Westerte <WesterteN@mail.ru>
2015-05-20 21:40:44 +0300

    entities were created

---
+++ workspace/airline/src/edu/vadimnesterenko/airline/logger/LoggerConfigurator.java
@@ -0,0 +1,10 @@
+package edu.vadimnesterenko.airline.logger;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class LoggerConfigurator {	
+	static {
+		new DOMConfigurator().doConfigure("config/log4j.xml", LogManager.getLoggerRepository());
+	}
+}
---
+++ workspace/five/src/com/epam/five/exception/LogicException.java
@@ -0,0 +1,10 @@
+package com.epam.five.exception;
+
+public class LogicException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5227494025426625814L;
+
+}
